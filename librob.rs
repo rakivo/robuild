efa067f6e71457d18386d0c6fadce93ff53c6bd7
Mark Tyrkba <awfulmarco@gmail.com>
2024-06-02 17:11:48 +0300

    New functionality: program that rebuilds yourself, therefore you need to bootstrap the librob.rs only once

--- librob.rs
+++ librob.rs
@@ -1,19 +1,32 @@
 use std::{
     fs::{
+        metadata,
         create_dir_all,
         read_dir,
         remove_dir_all,
-        remove_file
+        remove_file,
+        rename
     },
-    io::ErrorKind,
-    path::PathBuf,
+    any::Any,
+    time::SystemTime,
     collections::VecDeque,
+    path::{Path, PathBuf},
     thread::{self, JoinHandle},
-    process::{Command, Output, Stdio}
+    io::{ErrorKind, Result as ioResult},
+    process::{exit, Command, Output, Stdio},
 };
 
 const CMD_ARG: &str = if cfg!(windows) {"cmd"} else {"sh"};
 const CMD_ARG2: &str = if cfg!(windows) {"/C"} else {"-c"};
+
+#[macro_export]
+macro_rules! go_rebuild_yourself {
+    () => {{
+        let source_path = file!();
+        let args = std::env::args().collect::<Vec::<_>>();
+        Rob::go_rebuild_yourself(&args, &source_path).unwrap();
+    }}
+}
 
 macro_rules! colored {
     (r.$str: expr)  => { format!("\x1b[91m{}\x1b[0m", $str) };
@@ -23,12 +36,13 @@ macro_rules! colored {
 
 #[macro_export]
 macro_rules! log {
-    ($log_level: expr, $($args: expr), *) => {{
+    ($log_level: tt, $($args: expr), *) => {{
+        use LogLevel::*;
         let out = format!($($args), *);
         match $log_level {
-            LogLevel::PANIC => {
+            PANIC => {
                 let out = format!("{lvl} {f}:{l}:{c}: {out}",
-                                  lvl = $log_level, f = file!(),
+                                  lvl = LogLevel::$log_level, f = file!(),
                                   l = line!(), c = column!());
                 Rob::panic(&out)
             }
@@ -64,7 +78,7 @@ impl Dir {
     ///
     /// `Dir` iterates using the BFS algorithm,
     ///
-    /// if current element is file it returns it,
+    /// if current element is file `Dir` returns it,
     /// otherwise it iterates that directory and checkes for other files.
     pub fn new<P>(root: P) -> Dir
     where
@@ -107,7 +121,7 @@ pub enum LogLevel {
 
 impl std::fmt::Display for LogLevel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use LogLevel::*;
+        use self::LogLevel::*;
         match self {
             CMD   => write!(f, "[CMD]")?,
             INFO  => write!(f, "[INFO]")?,
@@ -131,6 +145,59 @@ impl Rob {
     #[inline]
     pub fn new() -> Rob {
         Rob(RobCommand::new())
+    }
+
+    fn needs_rebuild(bin: &str, srcs: &Vec::<&str>) -> ioResult::<bool> {
+        if !Rob::path_exists(bin) { return Ok(true) }
+
+        let bin_mod_time = Rob::get_last_modification_time(bin)?;
+        for src in srcs {
+            let src_mod_time = Rob::get_last_modification_time(src)?;
+            if src_mod_time > bin_mod_time {
+                return Ok(true)
+            }
+        } Ok(false)
+    }
+
+    // The implementation idea is stolen from https://github.com/tsoding/nobuild,
+    // which is also stolen from https://github.com/zhiayang/nabs
+    pub fn go_rebuild_yourself(args: &Vec::<String>, source_path: &str) -> ioResult::<()> {
+        assert!(args.len() >= 1);
+        let binary_path = &args[0];
+        let rebuild_is_needed = Rob::needs_rebuild(&binary_path, &vec![source_path])?;
+        if rebuild_is_needed {
+            let old_bin_path = format!("{binary_path}.old");
+            log!(INFO, "RENAMING: {binary_path} -> {old_bin_path}");
+            Rob::rename(binary_path, &old_bin_path)?;
+
+            if let Err(err) = Rob::new()
+                .append(&["rustc -o", binary_path, source_path])
+                .execute_sync()
+            {
+                log!(ERROR, "FAILED TO RENAME FILE: {old_bin_path}: {err}");
+                Rob::rename(&old_bin_path, binary_path)?;
+                exit(1);
+            }
+
+            match Rob::new()
+                .append(args)
+                .execute_sync()
+            {
+                Ok(_) => exit(0),
+                Err(err) => {
+                    log!(ERROR, "FAILED TO RESTART ROB FROM FILE: {binary_path}: {err}");
+                    exit(1);
+                }
+            }
+        } Ok(())
+    }
+
+    #[inline]
+    pub fn get_last_modification_time<P>(p: P) -> ioResult::<SystemTime>
+    where
+        P: AsRef::<Path>
+    {
+        metadata(p)?.modified()
     }
 
     #[inline]
@@ -158,16 +225,24 @@ impl Rob {
     }
 
     #[inline]
-    pub fn mkdir<P>(p: P) -> std::io::Result<()>
+    pub fn rename<P>(from: P, to: P) -> ioResult<()>
+    where
+        P: AsRef::<Path>
+    {
+        rename(from, to)
+    }
+
+    #[inline]
+    pub fn mkdir<P>(p: P) -> ioResult<()>
     where
         P: Into::<PathBuf>
     {
         create_dir_all(p.into())
     }
 
-    pub fn rm<P>(p: P) -> std::io::Result<()>
+    pub fn rm<P>(p: P) -> ioResult<()>
     where
-        P: Into::<PathBuf> + Clone
+        P: Into::<PathBuf> + ToOwned::<Owned = String>
     {
         if !Rob::path_exists(p.to_owned()) {
             return Err(ErrorKind::NotFound.into())
@@ -186,7 +261,7 @@ impl Rob {
     }
 
     fn log(lvl: LogLevel, out: &str, f: &str, l: u32, c: u32) {
-        use LogLevel::*;
+        use self::LogLevel::*;
         let out_with_info = format!("{f}:{l}:{c}: {out}");
         match lvl {
             CMD   => println!("{lvl} {out}"),
@@ -203,29 +278,43 @@ impl Rob {
         p.chars().take_while(|x| *x != '.').collect()
     }
 
+    /* TODO:
+    Change the way how we treating lines, i think we should introduce
+    some new function like `execute_last_sync` or something, that just moves command pointer
+    therefore following input will be treated as next line and so on, if ykwim
+    */
     /// Takes all of the args and pushes them to self.lines,
-    /// Each of the line will be executed as LINES, like you using cmd, for example:
+    /// Each call of the append function will be treated as one line of arguments, as in here:
     /// ```
-    /// let p = pathbuf!("dummy", "rakivo", "dummy.cpp");
+    /// let p = pathbuf!["dummy", "rakivo", "dummy.cpp"];
     /// let handle = Rob::new()
     ///     .append(&["clang++", "-o", "output", p.to_str().unwrap()])
     ///     .execute_sync();
     /// ```
-    /// Outputs:
+    /// It Outputs:
+    /// ```
     /// [CMD] clang++ -o output test/test1/test.cpp
     /// [INFO] [EMPTY]
+    /// ```
     ///
     /// Output is [EMPTY] because the program compiled successfully.
     #[inline]
-    pub fn append(&mut self, args: &[&str]) -> &mut Self {
-        self.0.lines.push(args.join(" "));
+    pub fn append<'a, I, S>(&mut self, args: I) -> &mut Self
+    where
+        I: IntoIterator::<Item = S>,
+        S: AsRef<str>
+    {
+        let line = args.into_iter()
+            .map(|arg| arg.as_ref().to_owned())
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        self.0.lines.push(line);
         self
     }
 
     fn format_out(out: &str) -> &str {
-        if out.is_empty() {
-            "[EMPTY]"
-        } else if out.ends_with('\n') {
+        if out.ends_with('\n') {
             &out[0..out.len() - 1]
         } else {
             out
@@ -235,12 +324,16 @@ impl Rob {
     fn process_output(out: &Output) {
         if out.status.success() {
             let stdout = String::from_utf8_lossy(&out.stdout);
-            let formatted = Rob::format_out(&stdout);
-            log!(LogLevel::INFO, "{formatted}");
+            if !stdout.is_empty() {
+                let formatted = Rob::format_out(&stdout);
+                log!(INFO, "{formatted}");
+            }
         } else {
             let stderr = String::from_utf8_lossy(&out.stderr);
-            let formatted = Rob::format_out(&stderr);
-            log!(LogLevel::ERROR, "{formatted}");
+            if !stderr.is_empty() {
+                let formatted = Rob::format_out(&stderr);
+                log!(ERROR, "{formatted}");
+            }
         }
     }
 
@@ -248,19 +341,19 @@ impl Rob {
     ///
     /// Simply creates `Command` for each line in self.lines and executes them.
     /// More about that: https://doc.rust-lang.org/std/process/struct.Command.html
-    pub fn execute_sync(&mut self) -> Vec::<Result::<Output, ()>> {
-        self.0.lines.iter().map(|line| {
-            log!(LogLevel::CMD, "{line}");
+    pub fn execute_sync(&mut self) -> ioResult::<Vec::<Output>> {
+        let mut ret = Vec::new();
+
+        for line in self.0.lines.iter() {
+            log!(CMD, "{line}");
             let out = Command::new(CMD_ARG)
                 .arg(CMD_ARG2)
                 .arg(&line)
-                .output()
-                .map_err(|err| log!(LogLevel::ERROR, "{err}"))?;
+                .output()?;
 
             Self::process_output(&out);
-
-            Ok(out)
-        }).collect()
+            ret.push(out);
+        } Ok(ret)
     }
 
     /// Non-blocking operation.
@@ -272,39 +365,45 @@ impl Rob {
     /// Returns handle that waits for each child and contains vector of the outputs.
     ///
     /// To conveniently wait for the returned children you can use Rob::wait_for_children
-    pub fn execute_async(&mut self) -> JoinHandle::<Vec::<std::io::Result::<Output>>> {
-        let children = self.0.lines.iter().map(|line| {
-            log!(LogLevel::CMD, "{line}");
-            Command::new(CMD_ARG)
+    pub fn execute_async(&mut self) -> ioResult::<JoinHandle::<Vec::<Output>>> {
+        let mut children = Vec::new();
+        for line in self.0.lines.iter() {
+            log!(CMD, "{line}");
+
+            let out = Command::new(CMD_ARG)
                 .arg(CMD_ARG2)
                 .arg(&line)
                 .stdout(Stdio::piped())
                 .stderr(Stdio::piped())
-                .spawn()
-                .map_err(|err| {
-                    log!(LogLevel::PANIC, "FAILED TO EXECUTE PROCESS: {err}")
-                }).unwrap()
-        }).collect::<Vec<_>>();
+                .spawn()?;
 
-        thread::spawn(move || {
+            children.push(out);
+        }
+
+        let handle = thread::spawn(move || {
             children.into_iter().map(|child| {
                 child.wait_with_output().map_err(|err| {
-                    log!(LogLevel::PANIC, "FAILED TO WAIT FOR CHILD: {err}");
+                    log!(PANIC, "FAILED TO WAIT FOR CHILD: {err}");
                     err
-                })
+                }).unwrap()
             }).collect()
-        })
+        });
+
+        Ok(handle)
     }
 
     /// Blocks the main thread and waits for all of the children.
-    pub fn wait_for_children(handle: JoinHandle::<Vec::<std::io::Result::<Output>>>) {
-        handle.join()
-            .unwrap()
+    pub fn wait_for_children<E>(handle: JoinHandle::<Vec::<ioResult::<Output>>>) -> Result<(), E>
+    where
+        E: From::<Box<dyn Any + Send>>
+    {
+        handle.join()?
             .iter()
-            .filter_map(|res| res.as_ref().ok())
-            .for_each(|out| {
-                Rob::process_output(&out);
-            });
+            .try_for_each(|res| {
+                if let Ok(out) = res {
+                    Rob::process_output(out);
+                } Ok(())
+            })
     }
 }
 
